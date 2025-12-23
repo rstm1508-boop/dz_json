db5fcd08e039c67abec3a1e9deb67c11a14c707a
rstm1508-boop <rstm1508@gmail.com>
2025-12-23 13:24:38 +0300

    Add files via upload

---
+++ dz_json.py
@@ -0,0 +1,28 @@
+import json
+
+
+def generate_report(json_file_path):
+
+    with open(json_file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+
+    report = {}
+
+    for task in data:
+        assignee = task['assignee']
+        if task['status'] == 'completed':
+
+            if assignee not in report:
+                report[assignee] = 0
+
+
+            report[assignee] += 1
+
+    return report
+
+
+# 
+if __name__ == "__main__":
+    result = generate_report('tasks.json')
+    print(result)
